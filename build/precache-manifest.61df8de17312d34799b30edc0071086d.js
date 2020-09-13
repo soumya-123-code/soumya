35672523c49f76466a226e40b8a5106df16547a9
Aukpro198 <info@aukpro.com>
2020-09-14 00:53:15 +0530

    tr

---
+++ build/precache-manifest.61df8de17312d34799b30edc0071086d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a48b37495f5986e0dae",
+    "url": "/trello-clone/static/css/main.c68b9877.chunk.css"
+  },
+  {
+    "revision": "2a48b37495f5986e0dae",
+    "url": "/trello-clone/static/js/main.2a48b374.chunk.js"
+  },
+  {
+    "revision": "77a8e5d759d4dcebcb23",
+    "url": "/trello-clone/static/js/runtime~main.77a8e5d7.js"
+  },
+  {
+    "revision": "73dc21cfe9c5cd77c72a",
+    "url": "/trello-clone/static/js/2.73dc21cf.chunk.js"
+  },
+  {
+    "revision": "b5e6925f6883389a9db53cae2a65a6bf",
+    "url": "/trello-clone/index.html"
+  }
+];
